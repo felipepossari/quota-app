8761998ae0798b224f55a7340723ba8d6791de39
felipepossari <fcpfelipe@hotmail.com>
2024-02-15 09:45:41 -0300

    Initial commit

---
+++ src/main/java/com/felipepossari/quota/QuotaAppApplication.java
@@ -0,0 +1,13 @@
+package com.felipepossari.quota;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuotaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuotaAppApplication.class, args);
+	}
+
+}
