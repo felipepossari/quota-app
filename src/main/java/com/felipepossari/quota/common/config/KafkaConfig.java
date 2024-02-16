ae3ad7569247e5785c17c888899c3ec876cd8709
felipepossari <fcpfelipe@hotmail.com>
2024-02-16 19:04:10 -0300

    Add kafka consumer config and user consumer first version

--- src/main/java/com/felipepossari/quota/common/config/KafkaConfig.java
+++ src/main/java/com/felipepossari/quota/common/config/KafkaConfig.java
@@ -2,11 +2,16 @@ package com.felipepossari.quota.common.config;
 
 import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaAdmin;
 import org.springframework.kafka.core.KafkaTemplate;
@@ -49,6 +54,25 @@ public class KafkaConfig {
     @Bean
     public KafkaTemplate<String, String> kafkaTemplate() {
         return new KafkaTemplate<>(getProducerFactory());
+    }
+
+    @Bean
+    public ConsumerFactory<String, String> getConsumerFactory() {
+        return new DefaultKafkaConsumerFactory<>(getConsumerMapConfig());
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, String> factory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(getConsumerFactory());
+        return factory;
+    }
+
+    private Map<String, Object> getConsumerMapConfig() {
+        return Map.of(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress,
+                ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class,
+                ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
     }
 
     private Map<String, Object> getProducerMapConfig() {
---
+++ src/main/java/com/felipepossari/quota/user/UserConsumer.java
@@ -0,0 +1,19 @@
+package com.felipepossari.quota.user;
+
+import com.felipepossari.quota.quota.QuotaService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class UserConsumer {
+
+    @KafkaListener(topics = "user", groupId = "user")
+    public void consumeUserMessage(String message){
+        log.info("User message received. Message: {}", message);
+    }
+}
